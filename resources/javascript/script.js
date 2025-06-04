79166cb536d085ac26c340cc00f0871829024efa
Alexander <alexander.slyfield@hotmail.com>
2025-06-04 18:58:15 +0100

    Add javascript to pause moving carosel, minor css adjustments

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,10 +1,39 @@
 const inputElements = document.querySelectorAll("input:not([type='submit']), textarea");
 const button = document.querySelector("button");
+const script = document.querySelector("head script:nth-of-type(2)")
 
 const resetInput = () => {
-    console.log("Ok its working here");
     inputElements.forEach(input => input.value = "");
 };
 
-button.onclick = resetInput;
+if (button) {
+    button.onclick = resetInput;
+};
+
+
+
+
+const ul = document.querySelectorAll("#skills ul");
+const div = document.querySelector("#skills>div")
+
+const pauseCarosel = () => {
+    if (script.getAttribute("src") === "/resources/javascript/script.js") {
+        for (let list of ul) {
+            list.style.animationPlayState = "paused";
+        };
+    }
+};
+
+const playCarosel = () => {
+    if (script.getAttribute("src") === "/resources/javascript/script.js") {
+        for (let list of ul) {
+            list.style.animationPlayState = "running";
+        };
+    };
+};
+
+if (div) {
+    div.onmouseenter = pauseCarosel;
+    div.onmouseleave = playCarosel;
+}
 
